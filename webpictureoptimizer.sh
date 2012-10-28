306d7799fd8ab85451a043c7fbe98fb3c562bac6
it-consulting-lorenz <stefan.lorenz@it-consulting-lorenz.de>
2012-10-28 19:35:40 +0100

    added optipng and some comments

--- webpictureoptimizer.sh
+++ webpictureoptimizer.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env sh
 # 
 
-cd /var/www/ && find . -iname \*.jp*g -type f | grep uploads | grep -v sysext| xargs -0 jpegoptim -n --strip-com --strip-exif --strip-icc
+cd /var/www/
+# find all jpegs in upload folder and optimize them w/o stripping metadata
+find . -iname \*.jp*g -type f | grep uploads | grep -v sysext| xargs -0 jpegoptim -n --strip-com --strip-exif --strip-icc
 
+exit 0;
+
+# Running optipng with highest optimization level on all foud png
+find . -iname \*.png -type f | grep uploads | grep -v sysext| xargs -0 optipng -o7
 exit 0;
