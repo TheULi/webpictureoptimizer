06d6b6cb044d5561eb92f329dc2abaaff7517195
it-consulting-lorenz <stefan.lorenz@it-consulting-lorenz.de>
2012-10-28 19:48:03 +0100

    added echo message and some more comments removed -n option fpr jpegpoptim

--- webpictureoptimizer.sh
+++ webpictureoptimizer.sh
@@ -3,10 +3,15 @@
 
 cd /var/www/
 # find all jpegs in upload folder and optimize them w/o stripping metadata
-find . -iname \*.jp*g -type f | grep uploads | grep -v sysext| xargs -0 jpegoptim -n --strip-com --strip-exif --strip-icc
+# output of find is filtered to paths with containing an uploads folder and NOT 
+# containing a Typo3 sysext folder.
+
+find . -iname \*.jp*g -type f | grep uploads | grep -v sysext| xargs -0 jpegoptim --strip-com --strip-exif --strip-icc
 
 exit 0;
 
-# Running optipng with highest optimization level on all foud png
+# Running optipng with highest optimization level on all found png
 find . -iname \*.png -type f | grep uploads | grep -v sysext| xargs -0 optipng -o7
+
+echo "All Done, see your bandwithmeter dropping"
 exit 0;
