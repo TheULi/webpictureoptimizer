bd02e7cfdc8349d14572f7fce34cb1b71f1c1b83
Stefan Lorenz <stefan.lorenz@it-consulting-lorenz.de>
2012-11-02 20:24:05 +0100

    removed some code remnants added advpng-run after optipng

--- webpictureoptimizer.sh
+++ webpictureoptimizer.sh
@@ -2,7 +2,7 @@
 # 
 
 cd /var/www/
-# find all jpegs in upload folder and optimize them w/o stripping metadata
+# find all jpegs in upload folder and optimize them w/o stripping copyright-metadata
 # output of find is filtered to paths with containing an uploads folder.
 #symlinks are followed
 
@@ -11,7 +11,10 @@ cd /var/www/
 
 
 # Running optipng with highest optimization level on all found png
-find . -iname \*.png -type f | grep uploads | grep -v sysext| xargs -0 optipng -o7
+find . -iname \*.png -type f | grep uploads | xargs -0 optipng -o7
 
+#advpng to compress png with 7z algorithm
+find . -iname \*.png -type f | grep uploads | xargs -0 advpng -2
+#tbd
 echo "All Done, see your bandwithmeter dropping. "
 exit 0;
