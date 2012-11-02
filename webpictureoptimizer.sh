780a3a2e6dd6cdd3164d31e635e9d3ad5f2b1bbb
stefan <stefan@stefan-desktop>
2012-11-02 19:53:00 +0100

    first successful version

--- webpictureoptimizer.sh
+++ webpictureoptimizer.sh
@@ -3,12 +3,11 @@
 
 cd /var/www/
 # find all jpegs in upload folder and optimize them w/o stripping metadata
-# output of find is filtered to paths with containing an uploads folder and NOT 
-# containing a Typo3 sysext folder.
+# output of find is filtered to paths with containing an uploads folder.
 #symlinks are followed
 
-find . -L -iname \*.jp*g -type f | grep uploads | grep -v sysext| xargs -0 jpegoptim --strip-com --strip-exif --strip-icc
-
+ 
+ find . -iname \*.jp*g  | grep -a -Z uploads | xargs -0jpegoptim -o -t -v --strip-com --strip-exif --strip-icc
 
 
 # Running optipng with highest optimization level on all found png
