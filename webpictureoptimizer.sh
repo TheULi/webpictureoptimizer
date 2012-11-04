e2f23b370d1cc632b7e2caa090127f9190aab182
Stefan Lorenz <stefan.lorenz@it-consulting-lorenz.de>
2012-11-04 15:06:05 +0100

    parellel threads supported minor bug fixes and corrections

--- webpictureoptimizer.sh
+++ webpictureoptimizer.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env sh
 # 
-# first some system information ist gathered
-$CPUCOUNT = grep -c processor /proc/cpuinfo
-echo $CPUCOUNT
+# first some system information ist gathered, on a quadcore, 3 threads will be in parallel use
+CPUCOUNT=`grep -c processor /proc/cpuinfo`
+THREADS=$(($CPUCOUNT-1))
+echo "$CPUCOUNT Cores found, using $THREADS threads parallel"
+
 
 # find all jpegs in upload folder and optimize them w/o stripping copyright-metadata
 # output of find is filtered to paths with containing an uploads folder.
 #symlinks are followed
   
  
-find -iname \*.jp*g  | grep -a -Z uploads | xargs -n 1 -P $CPUCOUNT jpegoptim -o -t -v --strip-com --strip-exif --strip-icc
+find -L -iname \*.jp*g  | grep -a -Z uploads | xargs -n 1 -P $THREADS jpegoptim -o -t -v --strip-com --strip-exif --strip-icc
 
 # Running optipng with highest optimization level on all found png
-find . -iname \*.png -type f | grep uploads | xargs optipng -o2
+find . -iname \*.png -type f | grep uploads | xargs -n 1 -P $THREADS optipng -o2
   
 #advpng to compress png with 7z algorithm
-find . -iname \*.png -type f | grep uploads | xargs -n 1 -P $CPUCOUNT pngout 
+find . -iname \*.png -type f | grep uploads | xargs -n 1 -P $THREADS pngout 
 #tbd
 echo "All Done, see your bandwithmeter dropping. "
 exit 0;
